7a8a6374098d27ccfb31f3e4f7f94bb0cb3f06e3
SamuilBachev <sbachev@yahoo.com>
2021-05-30 01:39:58 +0300

    start

---
+++ DungeonGame/Monsters/Monster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGame.Monsters
+{
+    abstract class Monster
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Health { get; set; }
+        public int Attak { get; set; }
+        public int Defence { get; set; }
+        public double AttakRate { get; set; }
+        public int Experience { get; set; }
+        
+    }
+}
---
+++ DungeonGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DungeonGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
