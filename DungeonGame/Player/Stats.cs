f2381ec3b5ab85ba0ed33b7927c171541e8d874f
SamuilBachev <sbachev@yahoo.com>
2021-06-25 06:56:20 +0300

    hero class

--- DungeonGame/Player/Stats.cs
+++ DungeonGame/Player/Stats.cs
@@ -7,52 +7,28 @@ using System.Threading.Tasks;
 
 namespace DungeonGame.Player
 {
-    class Stats
+    abstract class Stats
     {
-        public double Attack { get; set; }
-        public double ArmorRate { get; set; }
-        public int Level { get; set; }
-        public double XP { get; set; }
-        public double HP { get; set; }
-        public double MaxHP { get; set; }
+        public abstract double Attack { get; set; }
+        public abstract double Defence { get; set; }
+        public abstract double AtackRate { get; set; }
+        public abstract int Level { get; set; }
+        public abstract double XP { get; set; }
+        public abstract double Health { get; set; }
+        public abstract double MaxHealth { get; set; }
 
-        public Stats()
-        {
-            this.Attack = 0;
-            this.ArmorRate = 0;
-            this.Level = 0;
-            this.XP = 0;
-            this.HP = 0;
 
-        }
-
-        public Stats(double attack, double armor, int level, double hp)
-        {
-            this.Attack = attack;
-            this.ArmorRate = armor;
-            this.Level = level;
-            this.HP = hp;
-            this.XP = XP;
-            this.HP = hp;
-        }
-
-        public Stats(double attack, double armor, int level, double hp, double maxhp)
-        {
-            this.Attack = attack;
-            this.ArmorRate = armor;
-            this.Level = level;
-            this.HP = hp;
-            this.XP = XP;
-            this.HP = hp;
-            this.MaxHP = maxhp;
-        }
 
         public void LevelUp()
         {
             Console.Clear();
             XP = 0;
             Level++;
-            MaxHP += 10;
+            MaxHealth += 30;
+            Health = MaxHealth;
+            Attack += 5;
+
+
             Console.WriteLine("X────────────────────────────X");
             Box.Create($"You are now level {Level}.", 30);
             Console.WriteLine("X────────────────────────────X");
