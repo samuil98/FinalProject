655326156fea1f34f33379e4359d9d17529de435
SamuilBachev <sbachev@yahoo.com>
2021-06-28 20:22:56 +0300

    items

---
+++ DungeonGame/Heroes/IItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGame.Heroes
+{
+    interface IItem
+    {
+        public int Attack { get; set; }
+        public int Armor { get; set; }
+
+        void Equip()
+        {
+
+        }
+        void Use()
+        {
+            
+        }
+    }
+}
